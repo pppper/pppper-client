274ab33844bce2c359ea1211465c204db8edd2cd
park ye jin <dyddyd0514@naver.com>
2021-08-03 14:27:04 +0900

    Feat: global style

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 import App from './App';
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    box-sizing:border-box;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
