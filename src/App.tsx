10710e60bb64b4d6ab6cf8d8664c99661001565b
park ye jin <dyddyd0514@naver.com>
2021-08-03 12:37:49 +0900

    Chore

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CustomButton } from './components/Button';
+
+function App() {
+  return (
+    <div>
+      <CustomButton active={true} outlined={false}>
+        fffff
+      </CustomButton>
+    </div>
+  );
+}
+
+export default App;
