b746810d1e42077b6d99a9f9a118171ce14de636
park ye jin <dyddyd0514@naver.com>
2021-08-03 12:38:33 +0900

    Feat: color constant

---
+++ src/constants/color.ts
@@ -0,0 +1,14 @@
+export const defaultColors = {
+  primary1: '#6129FF',
+  primary2: '#7A59FF',
+  primary3: '#A697FF',
+  black: '#000000',
+  gray1: '#2C2A32',
+  gray2: '#555561',
+  gray3: '#9796A0',
+  gray4: '#CFCED4',
+  gray5: '#DFDFE7',
+  gray6: '#EFEEF3',
+  gray7: '#F7F7F8',
+  white: '#ffffff',
+};
