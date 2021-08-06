0016b9e4a061cd532668b58a61be45374f31aa1b
park ye jin <dyddyd0514@naver.com>
2021-08-06 14:57:29 +0900

    [feat] divided list component

---
+++ src/components/DividedList.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface DividedListProps {
+  list: string[];
+}
+export const DividedList: React.FC<DividedListProps> = (props) => {
+  const { list } = props;
+  return (
+    <ListContainer>
+      {list.map((label) => {
+        return (
+          <ListComponent key={label}>
+            <ListLabel>{label}</ListLabel>
+          </ListComponent>
+        );
+      })}
+    </ListContainer>
+  );
+};
+
+const ListContainer = styled.div`
+  display: table;
+  border-collapse: collapse;
+`;
+const ListComponent = styled.div`
+  display: table-row;
+  border-top: 0.5px solid;
+  border-bottom: 0.5px solid;
+  border-color: ${(props) => props.theme.colors.gray4};
+`;
+const ListLabel = styled.div`
+  font-family: 'SpoqaHanSansNeo';
+  font-size: 15px;
+  font-weight: bold;
+  font-style: normal;
+  line-height: 22px;
+  text-align: left;
+  color: #000000;
+  margin: 10px 0px 9.5px;
+`;
