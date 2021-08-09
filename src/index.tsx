d56a8715d3dd173c39a5ce0caf1d2293a079f3b8
eun-ko <koeun0529@gmail.com>
2021-08-09 14:47:20 +0900

    [feat] provide redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
 
 import { GlobalStyle } from './styles/Globalstyle';
 
 import { AlertContextProvider } from './providers/AlertProvider';
+import store from './modules/store';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <BrowserRouter>
-      <AlertContextProvider>
-        <App />
-      </AlertContextProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AlertContextProvider>
+          <App />
+        </AlertContextProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
