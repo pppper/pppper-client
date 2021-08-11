c7d0094f45141215df436b100d5e1d38fd032b69
park ye jin <dyddyd0514@naver.com>
2021-08-11 12:05:24 +0900

    [add] brand product and image slider

--- src/components/BrandIntroduction.tsx
+++ src/components/BrandIntroduction.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { CustomButton } from './Button';
 import { Margin } from './Margin';
@@ -10,17 +10,86 @@ interface BrandIntroProps {
   mainImage?: URL;
 }
 export const BrandIntroduction: React.FC<BrandIntroProps> = (props) => {
+  const [userSwipeDistance, setUserSwipeDistance] = useState(0);
+  const [sliderLocation, setSliderLocation] = useState(0);
+  var swipeStart = 0;
+  const sliderWidth = document.getElementsByClassName(
+    'brand-codi-image-slider'
+  )[0];
+  function translateDistance() {
+    const min = 0;
+    const max = window.innerWidth - 16 - sliderWidth.clientWidth;
+    if (-userSwipeDistance / 1.5 + sliderLocation < min)
+      return setSliderLocation(min);
+    else if (-userSwipeDistance / 1.5 + sliderLocation > max)
+      return setSliderLocation(max);
+    return setSliderLocation(-userSwipeDistance / 1.5 + sliderLocation);
+  }
+  useEffect(() => {
+    if (sliderWidth) translateDistance();
+  }, [userSwipeDistance]);
   return (
-    <Container>
+    <Container moveDistance={sliderWidth ? sliderLocation : 0}>
       <div className="brand-introduction-title">DEEPCOLLECTIVE</div>
       <Margin margin={24} />
-      <img />
+      <img
+        className="brand-introduction-main-image"
+        src="http://image.musinsa.com/images/plan_w_mobile_img/2021080913300600000002970.jpg"
+      />
       <Margin margin={17} />
       <div className="brand-introduction-subtitle">모두에게 깊게 빠져들다</div>
       <Margin margin={11} />
       <div className="brand-introduction-content">
         스토리가 있는 의류패션과 라이프 스타일로 또 다른 문화를 만들다. Street
         Culture의 다채로움을 깊이 있게 담아내어 재탄생시킨다.
+      </div>
+      <Margin margin={17} />
+      <div className="brand-codi-image-slider-background">
+        <div className="brand-codi-image-slider"></div>
+      </div>
+      <Margin margin={11} />
+      <div
+        className="brand-codi-image-wrapper "
+        onTouchStart={(e) => (swipeStart = e.changedTouches[0].pageX)}
+        onTouchEnd={(e) =>
+          setUserSwipeDistance(e.changedTouches[0].pageX - swipeStart)
+        }
+      >
+        <img className="brand-codi-image" />
+        <img className="brand-codi-image" />
+        <img className="brand-codi-image" />
+        <img className="brand-codi-image" />
+        <img className="brand-codi-image" />
+        <img className="brand-codi-image" />
+        <img className="brand-codi-image" />
+        <img className="brand-codi-image" />
+      </div>
+      <Margin margin={15} />
+      <div className="brand-product-preview-wrapper flex-row">
+        <BrandProductPreview
+          title="1967 JET BLACK JEANS [WIDE STRAIGHT]"
+          image=""
+          price={59000}
+          discount={20}
+        />
+        <BrandProductPreview
+          title="Strappy sandal(Deep sleep)_OK2AM21002BLK"
+          image=""
+          price={59000}
+          discount={20}
+        />
+        <BrandProductPreview
+          title="트러플 카우 티"
+          image=""
+          price={59000}
+          discount={20}
+        />
+        <BrandProductPreview
+          title="트러플 카우 티"
+          image=""
+          price={59000}
+          discount={20}
+        />
       </div>
       <Margin margin={17} />
       <div className="brand-introduction-buttoncontainer">
@@ -32,7 +101,11 @@ export const BrandIntroduction: React.FC<BrandIntroProps> = (props) => {
   );
 };
 
-const Container = styled.div`
+interface ISliderProps {
+  moveDistance: number;
+}
+
+const Container = styled.div<ISliderProps>`
   padding: 36px 0 32px;
   display: flex;
   flex-direction: column;
@@ -46,6 +119,10 @@ const Container = styled.div`
     line-height: 39px;
     letter-spacing: -0.3px;
     text-align: center;
+  }
+  .brand-introduction-main-image {
+    width: 100%;
+    object-fit: cover;
   }
   .brand-introduction-subtitle {
     font-size: 24px;
@@ -74,5 +151,111 @@ const Container = styled.div`
       height: 34px;
       width: 125px;
     }
+  }
+  .brand-codi-image-slider-background {
+    height: 2px;
+    width: calc(100vw - 16px);
+    margin-left: 16px;
+    box-sizing: border-box;
+    background-color: ${(props) => props.theme.colors.gray5};
+  }
+  .brand-codi-image-slider {
+    height: 2px;
+    width: calc(100% / 3);
+    z-index: 3;
+    background-color: ${(props) => props.theme.colors.primary2};
+    transform: translate3d(${(props) => props.moveDistance}px, 0px, 0px);
+    transition: transform 0.1s;
+  }
+  .brand-codi-image-wrapper {
+    display: flex;
+    flex-direction: row;
+    overflow: scroll;
+    flex-wrap: nowrap;
+    margin-left: 16px;
+    .brand-codi-image {
+      min-width: 135px;
+      min-height: 135px;
+      object-fit: cover;
+      margin-right: 4px;
+    }
+  }
+  .brand-product-preview-wrapper {
+    flex-wrap: wrap;
+    padding-left: 16px;
+  }
+`;
+
+interface IBrandProductPreview {
+  title: string;
+  image: string;
+  price: number;
+  discount: number | null;
+}
+const BrandProductPreview: React.FC<IBrandProductPreview> = (props) => {
+  const { title, image, price, discount } = props;
+  return (
+    <StyledBrandProductPreview>
+      <img className="preview-product-image" />
+      <div className="preview-product-info-wrapper flex-col">
+        <div className="preview-product-title">{title}</div>
+        <div className=" flex-row">
+          <div className="preview-product-price">
+            {price.toLocaleString()}원
+          </div>
+          <div className="preview-product-discount">{discount}%</div>
+        </div>
+      </div>
+    </StyledBrandProductPreview>
+  );
+};
+
+const StyledBrandProductPreview = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 164px;
+  margin-bottom: 4px;
+  margin-right: 8px;
+  align-items: center;
+  .preview-product-image {
+    max-height: 45px;
+    min-height: 45px;
+    min-width: 45px;
+  }
+  .preview-product-info-wrapper {
+    margin-left: 8px;
+  }
+  .preview-product-title {
+    font-size: 10px;
+    font-weight: 300;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 14px;
+    letter-spacing: normal;
+    text-align: left;
+    color: ${(props) => props.theme.colors.gray2};
+    word-break: break-all;
+    margin-bottom: 4px;
+  }
+  .preview-product-price {
+    font-family: SpoqaHanSansNeo;
+    font-size: 10px;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 13px;
+    letter-spacing: normal;
+    color: ${(props) => props.theme.colors.black};
+    margin-right: 4px;
+  }
+  .preview-product-discount {
+    font-family: SpoqaHanSansNeo;
+    font-size: 10px;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 13px;
+    letter-spacing: normal;
+    color: ${(props) => props.theme.colors.primary1};
   }
 `;
