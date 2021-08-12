7492b2b1d38535caf9a9362ac2e2339e106fa4bb
eun-ko <koeun0529@gmail.com>
2021-08-12 14:14:24 +0900

    [fix] fix vercel deployment error

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
+import { DefaultTheme, ThemeProvider } from 'styled-components';
+
+import NavigationBar from './components/base/gnb/GlobalNavigationBar';
+import { Margin } from './components/Margin';
 
 import StoreHomePage from './pages/store/StoreHomePage';
 import StoreCategoryPage from './pages/store/category/StoreCategoryPage';
 import ProductCollectionPage from './pages/store/category/ProductCollectionPage';
 import ProductDetailPage from './pages/store/ProductDetailPage';
-
-import { DefaultTheme, ThemeProvider } from 'styled-components';
-
-
-import { useAlert } from './lib/hooks/useAlert';
 import CodiGenerationPage from './pages/CodiGenerationPage';
 import SlidingMenuBarTestingPage from './pages/component-testing/SlidingMenuBarTestingPage';
-import ProductCollectionPage from './pages/store/category/ProductCollectionPage';
-import StoreCategoryPage from './pages/StoreCategoryPage';
-import StoreHomePage from './pages/StoreHomePage';
+
+import { useAlert } from './lib/hooks/useAlert';
+
 import { lightTheme } from './styles/theme';
-import NavigationBar from './components/base/gnb/GlobalNavigationBar';
-import { Margin } from './components/Margin';
 
 function App() {
   const [theme, setTheme] = useState<DefaultTheme>(lightTheme);
