268c98940dc4627cfc02b1496a7c6e2cf6b3d86a
park ye jin <dyddyd0514@naver.com>
2021-08-12 16:45:39 +0900

    [add] product interface

--- src/types/product.d.ts
+++ src/types/product.d.ts
@@ -1,19 +1,19 @@
-import { IApiProduct } from '../lib/api/product/type';
-
 interface IProduct {
   id: number;
-  price: number;
   title: string;
-  tags: string[];
+  price: number;
+  originalPrice: number;
   brandId: number;
   brandName: string;
   codiImage: string;
-  mainImage: string;
-  categoryId: number;
-  likesCount: number;
-  originalPrice: number;
-  bookmarksCount: number;
   detailImages: string[];
+  mainImage: string;
+  sizeArray: string[];
+  tags: string[];
+
+  bookmarksCount: number;
+  likesCount: number;
+  grade: number;
 }
 
 export default IProduct;
